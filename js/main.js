2b17ed4a96af80a8b40707926421c16604f0caf6
Joao <joaopedroolive@hotmail.com>
2023-11-27 11:00:53 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -10,85 +10,154 @@ const MostraAno = document.getElementById("showAno")
 const MostraMes = document.getElementById("showMes")
 const MostraDia = document.getElementById("showDias")
 
-Mybtn.addEventListener("click", contagem);
+Mybtn.addEventListener("click", calculaData);
+const label = document.getElementsByTagName("label");
+const inputs = document.getElementsByTagName("input");
+const aviso = document.getElementsByClassName("warning");
 
 function basicValidation(){
-  const label = document.getElementsByTagName("label");
-  const inputs = document.getElementsByTagName("input");
-  const aviso = document.getElementsByClassName("warning");
   
-  
+  // Verifica se os campos estão vazios!
   if(dias.value == "" && mes.value == "" && ano.value == "")
   {
     // For para percorrer todas as classes de "aviso",todos os elemntos com a tag "label" e "input e assim exibir o warning e mudar as cores necessárias
-    for(let i = 0; i <=2; i++){
+    for(let i = 0; i <3; i++){
       aviso[i].innerHTML= "This fiel is required";
-      aviso[i].style.color ="#FF5757";
       label[i].style.color = "#FF5757";
+      inputs[i].style.border = "2px solid #FF5757";
       inputs[i].style.backgroundColor = "#F0F0F0";
 
     }
+  }
+    else{
+      for(let i = 0; i <3; i++){
+        aviso[i].innerHTML= "";
+        inputs[i].style.border = "2px solid #DBDBDB";
+        label[i].style.color = "#716F6F";
+        inputs[i].style.backgroundColor = "#FFF";
+      }
+
+      validaEntrada();
     }
 
-    else{
-      for(let i = 0; i <=2; i++){
-        aviso[i].innerHTML= "";
-        label[i].style.color = "#716F6F";
-        inputs[i].backgroundColor = "#FFF";
+}
+
+function validaEntrada(){
+    // Valida entrada dos inputs
+    const atual = new Date();
+
+    if(dias.value > 31 || dias.value <= 0){
+      errorData();
+    }
+    else if(mes.value > 12 || mes.value <= 0)
+    {
+      aviso[1].innerHTML = "Must be a valid month";
+
+      for (let i = 0; i < 3; i++) {
+      label[i].style.color = "#FF5757";
+      inputs[i].style.border = "2px solid #FF5757";
       }
     }
+
+    else if(ano.value > atual.getFullYear() || ano.value <= 0)
+    {
+      aviso[2].innerHTML = "Must be in the past";
+
+      for (let i = 0; i < 3; i++) {
+      label[i].style.color = "#FF5757";
+      inputs[i].style.border = "2px solid #FF5757";
+      }
+    }
+
+    else {
+      limparMensagens();
+      verificaAnoBissexto();
+  }
+ 
 }
 
-function contagem(){
+function verificaAnoBissexto() {
+
+  if (anoBissexto(ano.value)) 
+  {
+    if (mes.value == 2 && dias.value > 29) 
+    {
+        errorData();
+    } 
+      else 
+      {
+        calculaData();
+      }  
+  } 
+    else 
+  {
+    if (mes.value == 2 && dias.value > 28) 
+    {
+      errorData();
+    } 
+    else 
+    {
+      const mesesCom30dias = [4, 6, 9, 11];
+      const mesesCom31dias = [1, 3, 5, 7, 8, 10, 12]; 
+
+      const mesNumerico = parseInt(mes.value, 10);
+      if(mesesCom31dias.includes(mesNumerico) && dias.value <= 31){
+        calculaData();
+      }
+      else if(mesesCom30dias.includes(mesNumerico) && dias.value <=30)
+      {
+        calculaData();
+      }
+      else{
+        errorData();
+      }
+    }
+  }
+}
+
+function calculaData() {
+  let dataUsuario = new Date(ano.value, mes.value, dias.value);
+
+  let dataAtual = new Date();
+
+  let diferenca = dataAtual.getTime() - dataUsuario.getTime();
+  let anos = Math.floor(diferenca / (1000 * 60 * 60 * 24 * 365.25));
   
-//   // Pega a data atual
-//   const hoje = new Date();
+  let diasTotais = Math.floor(diferenca / (1000 * 60 * 60 * 24));
 
-//   // Pega a data digitada pelo usuario
-//   const dataNiver = new Date(`${ano.value},${mes.value}, ${dias.value}`);
+  const meses = Math.floor((diasTotais % 365) / 30);
+  const diffDias = Math.floor((diasTotais % 365) / 12);
 
-//   // Faz a diferença entre ano informado e atual
-//   const idade = Math.abs((dataNiver.getFullYear() - hoje.getFullYear()).toString().padStart(2,'0'));
+  console.log(anos, meses, diffDias)
 
-//   // Faz a diferença do mes informado e atual 
-//   const mesDiff = Math.abs(((dataNiver.getMonth() + 1)- hoje.getMonth()).toString().padStart(2,'0'));
-
-// // Calcula a diferença em milissegundos entre as duas datas
-// const diferencaEmMilissegundos = hoje - dataNiver;
-
-// // Converte a diferença em milissegundos para dias
-// const diferencaEmDias = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
-
-
-//   console.log(diferencaEmDias);
-//   console.log(idade);
-//   console.log(mesDiff);
-
-// Pega a data atual
-const hoje = new Date();
-
-const year = parseInt(ano.value);
-const month = parseInt(mes.value) - 1; 
-const dia = parseInt(dias.value);
-
-const dataNiver = new Date(ano, month, dia);
-
-// Calcula a diferença em milissegundos entre as duas datas
-const diferencaSegundos = (dataNiver - hoje)/1000 ;
-
-// Converte a diferença em milissegundos para dias
-const diferencaEmDias = Math.floor(diferencaSegundos / 3600 / 24);
-
-// Calcula a diferença em anos, meses e dias
-const diffAnos = hoje.getFullYear() - year;
-const diffMeses = (hoje.getMonth() - month).toString().padStart(2,'0');
-const diffDias = (hoje.getDate() - dia).toString().padStart(2,'0');
-
-console.log(diferencaEmDias);
-
-MostraAno.innerHTML = diffAnos;
-MostraMes.innerHTML = diffMeses;
-MostraDia.innerHTML = diffDias;
-
+  MostraAno.innerHTML = anos;
+  MostraMes.innerHTML = meses;
+  MostraDia.innerHTML = diffDias;
 
 }
+
+function errorData() {
+  aviso[0].innerHTML = "Must be a valid day";
+
+  for (let i = 0; i < 3; i++) {
+    label[i].style.color = "#FF5757";
+    inputs[i].style.border = "2px solid #FF5757";
+  }
+}
+
+function limparMensagens() {
+  aviso[0].innerHTML = "";
+  for (let i = 0; i <= 2; i++) {
+    label[i].style.color = "#716F6F";
+    inputs[i].style.border = "2px solid #DBDBDB";
+  }
+}
+
+function anoBissexto(ano) {
+  if ((ano % 400 === 0) || (ano % 4 === 0 && ano % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
